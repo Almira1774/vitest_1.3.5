9f5c6c2f84391f1a57ce1bd86b2ae1f77401ef43
Almira1774 <bezzbashennaya@gmail.com>
2026-03-17 14:50:40 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import Title from './components/Title';
+
+function App() {
+
+  return (
+    <>
+    <Title />
+    </>
+  );
+}
+
+export default App;
